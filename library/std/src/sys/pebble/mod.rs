2296ca70e5b05a256742248c09270f274721c35b
Isaac Woods <isaacwoods.home@gmail.com>
2020-12-24 13:39:09 +0000

    Flesh out Pebble's std implementation a little

--- library/std/src/sys/pebble/mod.rs
+++ library/std/src/sys/pebble/mod.rs
@@ -1,5 +1,10 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
+use crate::io as std_io;
+use crate::os::raw::c_char;
+
+pub use crate::sys_common::os_str_bytes as os_str;
+
 pub mod alloc;
 pub mod args;
 pub mod cmath;
@@ -23,5 +28,71 @@ pub mod thread_local_dtor;
 pub mod thread_local_key;
 pub mod time;
 
-mod common;
-pub use common::*;
+#[cfg(not(test))]
+pub fn init() {
+    alloc::ALLOCATOR.lock().init();
+}
+
+pub fn unsupported<T>() -> std_io::Result<T> {
+    Err(unsupported_err())
+}
+
+pub fn unsupported_err() -> std_io::Error {
+    std_io::Error::new(std_io::ErrorKind::Other, "operation not supported on this platform")
+}
+
+pub fn decode_error_kind(_code: i32) -> crate::io::ErrorKind {
+    crate::io::ErrorKind::Other
+}
+
+pub fn abort_internal() -> ! {
+    // TODO: this should call an `exit` syscall or something instead
+    core::intrinsics::abort();
+}
+
+/// This is called by the panic runtime to abort.
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn __rust_abort() {
+    abort_internal()
+}
+
+pub fn hashmap_random_keys() -> (u64, u64) {
+    (1, 2)
+}
+
+// This enum is used as the storage for a bunch of types which can't actually
+// exist.
+// TODO: why can't we use the never type?
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+pub enum Void {}
+
+pub unsafe fn strlen(mut s: *const c_char) -> usize {
+    // SAFETY: The caller must guarantee `s` points to a valid 0-terminated string.
+    unsafe {
+        let mut n = 0;
+        while *s != 0 {
+            n += 1;
+            s = s.offset(1);
+        }
+        n
+    }
+}
+
+pub mod memchr {
+    pub use core::slice::memchr::{memchr, memrchr};
+}
+
+// TODO: document how this differs from normal platforms
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // This resolves to `std::rt::lang_start`
+    extern "C" {
+        fn main(argc: isize, argc: *const *const c_char) -> i32;
+    }
+
+    let result = unsafe { main(0, 0x0 as *const *const c_char) };
+    // TODO: actually exit the program
+    loop {}
+}
--- library/std/src/sys/pebble/mutex.rs
+++ library/std/src/sys/pebble/mutex.rs
@@ -1,4 +1,71 @@
-use crate::cell::Cell;
+use crate::cell::{Cell, UnsafeCell};
+use crate::sync::atomic::{spin_loop_hint, AtomicBool, Ordering};
+use core::ops::{Deref, DerefMut};
+
+#[derive(Default)]
+pub struct SpinMutex<T> {
+    lock: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T: Send> Send for SpinMutex<T> {}
+unsafe impl<T: Send> Sync for SpinMutex<T> {}
+
+pub struct SpinMutexGuard<'a, T: 'a> {
+    mutex: &'a SpinMutex<T>,
+}
+
+impl<'a, T> !Send for SpinMutexGuard<'a, T> {}
+unsafe impl<'a, T: Sync> Sync for SpinMutexGuard<'a, T> {}
+
+impl<T> SpinMutex<T> {
+    pub const fn new(value: T) -> Self {
+        SpinMutex { lock: AtomicBool::new(false), value: UnsafeCell::new(value) }
+    }
+
+    #[inline(always)]
+    pub fn lock(&self) -> SpinMutexGuard<'_, T> {
+        loop {
+            match self.try_lock() {
+                Some(guard) => return guard,
+                None => {
+                    while self.lock.load(Ordering::Relaxed) {
+                        spin_loop_hint()
+                    }
+                }
+            }
+        }
+    }
+
+    #[inline(always)]
+    pub fn try_lock(&self) -> Option<SpinMutexGuard<'_, T>> {
+        if !self.lock.compare_and_swap(false, true, Ordering::Acquire) {
+            Some(SpinMutexGuard { mutex: self })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> Deref for SpinMutexGuard<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.mutex.value.get() }
+    }
+}
+
+impl<'a, T> DerefMut for SpinMutexGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.mutex.value.get() }
+    }
+}
+
+impl<'a, T> Drop for SpinMutexGuard<'a, T> {
+    fn drop(&mut self) {
+        self.mutex.lock.store(false, Ordering::Release)
+    }
+}
 
 pub struct Mutex {
     // This platform has no threads, so we can use a Cell here.
